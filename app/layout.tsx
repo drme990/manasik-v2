f43c42f3d8c0d11ac8d76222d7c7709de2714652
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2026-02-14 23:56:01 +0200

    Add new english font Satoshi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
 import localFont from 'next/font/local';
 import OurThemeProvider from '@/components/providers/theme-provider';
 import { CurrencyProvider } from '@/components/providers/currency-provider';
@@ -7,39 +6,89 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import './globals.css';
 
-// Poppins font for English
-const poppins = Poppins({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
+// Satoshi font for English
+const satoshi = localFont({
+  src: [
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-Italic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Satoshi/Satoshi-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-satoshi',
   display: 'swap',
-  variable: '--font-poppins',
 });
 
 // ExpoArabic font for Arabic
 const expoArabic = localFont({
   src: [
     {
-      path: '../public/fonts/ExpoArabic-Light.ttf',
+      path: '../public/fonts/ExpoArabic/ExpoArabic-Light.ttf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/ExpoArabic-Book.ttf',
+      path: '../public/fonts/ExpoArabic/ExpoArabic-Book.ttf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/ExpoArabic-Medium.ttf',
+      path: '../public/fonts/ExpoArabic/ExpoArabic-Medium.ttf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/ExpoArabic-SemiBold.ttf',
+      path: '../public/fonts/ExpoArabic/ExpoArabic-SemiBold.ttf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/ExpoArabic-Bold.otf',
+      path: '../public/fonts/ExpoArabic/ExpoArabic-Bold.otf',
       weight: '700',
       style: 'normal',
     },
@@ -151,7 +200,7 @@ export default async function RootLayout({
   const locale = await getLocale();
   const messages = await getMessages();
   const direction = locale === 'ar' ? 'rtl' : 'ltr';
-  const fontClass = locale === 'ar' ? expoArabic.variable : poppins.variable;
+  const fontClass = locale === 'ar' ? expoArabic.variable : satoshi.variable;
 
   return (
     <html
@@ -162,7 +211,7 @@ export default async function RootLayout({
       className={fontClass}
     >
       <body
-        className={`antialiased ${locale === 'ar' ? 'font-expo-arabic' : 'font-poppins'}`}
+        className={`antialiased ${locale === 'ar' ? 'font-expo-arabic' : 'font-satoshi'}`}
         suppressHydrationWarning
       >
         <NextIntlClientProvider locale={locale} messages={messages}>
